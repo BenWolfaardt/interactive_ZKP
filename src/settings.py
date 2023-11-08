7d23b33b3e5e3fcd7e27ee998e35f3cfd5b3cadb
Ben Wolfaardt <benjaminwolfaardt@gmail.com>
2023-11-08 16:59:09 +0100

    Fix approach1

--- src/settings.py
+++ src/settings.py
@@ -4,8 +4,6 @@ from enum import Enum
 
 from envyaml import EnvYAML
 
-from src.lib import PublicVariableGenerator
-
 
 class Flavour(str, Enum):
     EXPONENTIATIONS = "exponentiations"
@@ -30,30 +28,12 @@ class Settings:
     def __init__(self, yaml_config: EnvYAML) -> None:
         self.config = yaml_config
         # TODO some checking on input data
-        self.q: int = self.config["public_variables"]["p"]
+        self.q: int = self.config["public_variables"]["q"]
         self.g: int = self.config["public_variables"]["g"]
         self.h: int = self.config["public_variables"]["h"]
         self.flavour: Flavour = self.config["implementaion"]["flavour"]
         self.bits: int = self.config["implementaion"]["bits"]
-        self.default_values: bool = self.config["implementaion"]["use_preconfigured_env"]
         self.log_level: str = self.config["logging"]["level"]
-
-    def set_public_variables(self) -> None:
-        generator = PublicVariableGenerator()
-
-        # TODO get approach from config
-        approach = 1
-
-        if approach == 1:
-            approach_1 = generator.Approach1(generator)
-            self.q, self.g, self.h = approach_1.get_public_variables()
-            # TODO logging.DEBUG
-            print(f"Approach 1: {self.q=}, {self.g=}, {self.h=}")
-        else:
-            approach_1 = generator.Approach1(generator)
-            self.q, self.g, self.h = approach_1.get_public_variables()
-            # TODO logging.DEBUG
-            print(f"Approach 1: {self.q=}, {self.g=}, {self.h=}")
 
 
 if __name__ == "__main__":
@@ -63,5 +43,4 @@ if __name__ == "__main__":
     print(f"{settings.h=}")
     print(f"{settings.flavour=}")
     print(f"{settings.bits=}")
-    print(f"{settings.default_values=}")
     print(f"{settings.log_level=}")

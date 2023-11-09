5b7611b11cd6bcb858ee2ae8d7883115c4270927
Ben Wolfaardt <benjaminwolfaardt@gmail.com>
2023-11-09 11:44:57 +0100

    Add pydantic data model and improve cli

--- src/client.py
+++ src/client.py
@@ -3,7 +3,7 @@ import logging
 import random
 import sys
 
-from collections import defaultdict
+from time import sleep
 
 import grpc
 
@@ -15,8 +15,15 @@ from proto.zkp_auth_pb2 import (
     RegisterRequest,
 )
 from proto.zkp_auth_pb2_grpc import AuthStub
+from pydantic import BaseModel
 
 from src.settings import Flavour, Settings, log_level_mapping, zkp_settings
+
+
+class ClientUserData(BaseModel):
+    user_name: str
+    x: int
+    k: int
 
 
 class Client:
@@ -32,8 +39,8 @@ class Client:
         self.bits: int = 0
         self.log_level: str = "info"
         # user session details
-        self.user: str | None = None
-        self.user_data: dict = defaultdict(set)
+        self.user: str
+        self.user_data: dict[str, ClientUserData] = {}
 
     def set_variables(self) -> None:
         self.settings = zkp_settings
@@ -52,7 +59,6 @@ class Client:
             self._logger.setLevel(log_level_mapping[self.log_level])
         return self._logger
 
-    # TODO: data validation with pydantic?
     def Register(self, stub: AuthStub) -> None:
         self.user = input("Enter your username: ")
 
@@ -62,9 +68,6 @@ class Client:
                 break
             except ValueError:
                 self.logger.error(f"{self.user} your password must be an integer.")
-
-        # self.user = "Bob"
-        # x = 123456789
 
         y1: int = pow(self.g, x, self.p)
         y2: int = pow(self.h, x, self.p)
@@ -88,32 +91,23 @@ class Client:
             self.logger.exception(f"Caught an exception: {e}")
             sys.exit()
 
-        self.user_data[self.user] = {
-            "user_name": self.user,
-            "x": x,
-        }
+        self.user_data[self.user] = ClientUserData(user_name=self.user, x=x)
 
     def CreateAuthenticationChallenge(self, stub: AuthStub) -> AuthenticationChallengeResponse:
-        if self.user:
-            user: str = self.user
-
         k: int = random.randint(1, self.q - 1)
         self.logger.debug(f"The random {k=}")
         r1: int = pow(self.g, k, self.p)
         r2: int = pow(self.h, k, self.p)
 
+        self.user_data[self.user].k = k
+
         request = AuthenticationChallengeRequest(
-            user=user,
+            user=self.user,
             r1=r1,
             r2=r2,
         )
 
-        # TODO should this be done elsewhere?
-        if self.user not in self.user_data:
-            self.user_data[self.user] = {}
-        self.user_data[self.user]["k"] = k
-
-        self.logger.debug(f"The authentication challenge variables for {user} are: {k=}, {r1=}, {r2=}")
+        self.logger.debug(f"The authentication challenge variables for {self.user} are: {k=}, {r1=}, {r2=}")
         response: AuthenticationChallengeResponse = stub.CreateAuthenticationChallenge(request)
         return response
 
@@ -122,8 +116,8 @@ class Client:
     ) -> None:
         auth_id: str = authentication_challenge_response.auth_id
         c: int = authentication_challenge_response.c
-        k: int = self.user_data[self.user]["k"]
-        x: int = self.user_data[self.user]["x"]
+        k: int = self.user_data[self.user].k
+        x: int = self.user_data[self.user].x
 
         s: int = (k - c * x) % self.q
         self.logger.debug(f"The computed {s=}")
@@ -149,43 +143,42 @@ class Client:
 
 
 def main() -> None:
-    # action = None
+    registered = False
 
     parser = argparse.ArgumentParser(description="Client for Chaum-Pederson Zero-Knowledge Proof")
     parser.add_argument("mode", choices=["local", "docker"], help="Choose 'local' or 'docker'")
 
     args = parser.parse_args()
 
-    if args.mode == "docker":
-        host = "server"
-    elif args.mode == "local":
-        host = "localhost"
+    host = "server" if args.mode == "docker" else "localhost"
 
     while True:
-        with grpc.insecure_channel(f"{host}:50051") as channel:
-            stub: AuthStub = AuthStub(channel)
-            c.Register(stub)
-            authentication_challenge_response = c.CreateAuthenticationChallenge(stub)
-            c.VerifyAuthentication(stub, authentication_challenge_response)
+        if not registered:
+            print("You need to register before logging in.")
+            action = "register"
+        else:
+            action = input("Please choose your desired action ('register', 'login' or 'exit'):\n")
 
-        # while action not in ["register", "login", "exit"]:
-        #     action = input("Please choose your desired action ('register', 'login' or 'exit'):\n")
+        if action == "register":
+            with grpc.insecure_channel(f"{host}:50051") as channel:
+                stub: AuthStub = AuthStub(channel)
+                c.Register(stub)
+                registered = True
+        elif action == "login":
+            if not registered:
+                print("Please register before trying to log in.")
+            else:
+                with grpc.insecure_channel(f"{host}:50051") as channel:
+                    stub = AuthStub(channel)
+                    authentication_challenge_response = c.CreateAuthenticationChallenge(stub)
+                    c.VerifyAuthentication(stub, authentication_challenge_response)
+        elif action == "exit":
+            print("Exiting the application.")
+            sys.exit(0)
+        else:
+            print("Invalid action. Please choose 'register', 'login', or 'exit'.")
 
-        # if action == "register":
-        #     with grpc.insecure_channel(f"{host}:50051") as channel:
-        #         stub: AuthStub = AuthStub(channel)
-        #         c.Register(stub)
-        # elif action == "login":
-        #     # TODO update so that it knows what user we are using
-        #     with grpc.insecure_channel(f"{host}:50051") as channel:
-        #         stub = AuthStub(channel)
-        #         authentication_challenge_response = c.CreateAuthenticationChallenge(stub)
-        #         c.VerifyAuthentication(stub, authentication_challenge_response)
-        # elif action == "exit":
-        #     sys.exit(1)
-
-        # action = None
-        # sleep(1)
+        sleep(1)
 
 
 if __name__ == "__main__":
